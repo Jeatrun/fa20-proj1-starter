dd1ff8ed3a8f782d5c81c99afd82310011b13c55
KodomoHan <2821117052@qq.com>
2024-03-30 15:15:22 +0800

    A2

---
+++ imageloader.c
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/************************************************************************
+**
+** NAME:        imageloader.c
+**
+** DESCRIPTION: CS61C Fall 2020 Project 1
+**
+** AUTHOR:      Dan Garcia  -  University of California at Berkeley
+**              Copyright (C) Dan Garcia, 2020. All rights reserved.
+**              Justin Yokota - Starter Code
+**				YOUR NAME HERE
+**
+**
+** DATE:        2020-08-15
+**
+**************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+#include "imageloader.h"
+
+//Opens a .ppm P3 image file, and constructs an Image object. 
+//You may find the function fscanf useful.
+//Make sure that you close the file with fclose before returning.
+Image *readData(char *filename) 
+{
+	//YOUR CODE HERE
+	
+	
+	//arguments of an image structure
+
+	uint32_t colorbits;
+	Image* img = (Image*)malloc(sizeof(Image));
+
+	//open the file
+	FILE *fp = fopen(strcat(filename,".ppm"),"r");
+
+	//read the header
+	fscanf(fp, "P3\n%"SCNu32" %"SCNu32"\n255\n",&img->cols,&img->rows);
+
+	//create a 2D array of colors
+	int totalpix = img->rows * img->cols;
+	img->image = (Color**)malloc(sizeof(Color*) * totalpix);
+	for (int i = 0; i < totalpix; i++) {
+		*(img->image+i)= (Color*)malloc(sizeof(Color));
+		Color* pixel = *(img->image + i);
+		fscanf(fp, " %hhu  %hhu  %hhu   ", &pixel->R, &pixel->G, &pixel->B);
+		
+
+	}
+	fclose(fp);
+
+	return img;
+}
+
+//Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
+void writeData(Image *image)
+{
+	//YOUR CODE HERE
+	
+	//print the header
+	printf("P3\n%u %u\n255\n",image->cols,image->rows);
+	//print the image data
+	Color** p=image->image;
+	for(int i=0;i<image->rows;i++)
+	{
+	    for(int j=0;j<image->cols-1;j++)
+		{
+			printf(" %hhu  %hhu  %hhu   ",(*p)->R,(*p)->G,(*p)->B);
+			p++;
+		}
+		//print the last col with \n
+		printf(" %hhu  %hhu  %hhu\n",(*p)->R,(*p)->G,(*p)->B);
+		p++;
+	}
+
+
+}
+
+//Frees an image
+void freeImage(Image *image)
+{
+	//YOUR CODE HERE
+	Color** p=image->image;
+	//free the pointers of color** image
+	for(int i=0;i<image->rows*image->cols;i++)
+	{
+	    
+		free(*p);
+		p++;
+	}
+	//free the color** image
+	free(image->image);
+	free(image);
+}
---
+++ steganography.c
@@ -0,0 +1,111 @@
+/************************************************************************
+**
+** NAME:        steganography.c
+**
+** DESCRIPTION: CS61C Fall 2020 Project 1
+**
+** AUTHOR:      Dan Garcia  -  University of California at Berkeley
+**              Copyright (C) Dan Garcia, 2020. All rights reserved.
+**				Justin Yokota - Starter Code
+**				YOUR NAME HERE
+**
+** DATE:        2020-08-23
+**
+**************************************************************************/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "imageloader.h"
+
+//Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
+Color *evaluateOnePixel(Image *image, int row, int col)
+{
+	//YOUR CODE HERE
+	
+	//get the color** image
+	Color ** p = image->image;
+
+	//get the B value of the given pixel
+	int currentIndex=row*image->cols+col;
+	uint8_t blue=p[currentIndex]->B;
+	
+	//decode it 
+	uint8_t LSB=blue&1;
+
+	//create a new Color
+	Color *newColor=malloc(sizeof(Color));
+	if(LSB){
+		newColor->R=255;
+		newColor->G=255;
+		newColor->B=255;
+	}else{
+		newColor->R=0;
+		newColor->G=0;
+		newColor->B=0;
+	}
+	return newColor;
+
+
+}
+
+//Given an image, creates a new image extracting the LSB of the B channel.
+Image *steganography(Image *image)
+{
+	//YOUR CODE HERE
+	//create a new image
+	Image *newImage=malloc(sizeof(Image));
+	newImage->cols=image->cols;
+	newImage->rows=image->rows;
+	newImage->image=malloc(sizeof(Color*)*image->rows*image->cols);
+
+	//access every pixels
+	for(int i=0;i<image->rows;i++){
+		for(int j=0;j<image->cols;j++){
+			//evaluate the lsb 
+			Color *newColor=malloc(sizeof(Color));
+			newColor=evaluateOnePixel(image,i,j);
+			//set the new color to the new image
+			newImage->image[i*image->cols+j]=newColor;
+		}
+
+
+	}
+
+	return newImage;
+
+}
+
+/*
+Loads a file of ppm P3 format from a file, and prints to stdout (e.g. with printf) a new image, 
+where each pixel is black if the LSB of the B channel is 0, 
+and white if the LSB of the B channel is 1.
+
+argc stores the number of arguments.
+argv stores a list of arguments. Here is the expected input:
+argv[0] will store the name of the program (this happens automatically).
+argv[1] should contain a filename, containing a file of ppm P3 format (not necessarily with .ppm file extension).
+If the input is not correct, a malloc fails, or any other error occurs, you should exit with code -1.
+Otherwise, you should return from main with code 0.
+Make sure to free all memory before returning!
+*/
+int main(int argc, char **argv)
+{
+	//YOUR CODE HERE
+	//get the filename
+	char *filename=argv[1];
+	//read the file,get the image
+	Image *image=readData(filename);
+
+	//evaluate the image,decode it,creat a new one
+	Image *newImage=steganography(image);
+	
+	//print the new image out
+	writeData(newImage);
+
+
+	//free all memories
+	freeImage(newImage);
+	freeImage(image);
+}
